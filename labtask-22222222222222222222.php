e5f5e7c039ae49f7b41ced972cca374ffa034c11
Lima112001 <115709631+Lima112001@users.noreply.github.com>
2023-02-12 16:57:16 +0600

    Add files via upload

---
+++ labtask-22222222222222222222.php
@@ -0,0 +1,65 @@
+
+</html><!DOCTYPE html>
+<html>
+<head>
+
+<title></title>
+</head>
+<body>
+<h4>Design the following HTML form and perform appropriate validations </h4>
+<fieldset>
+<legend><B>REGISTRATION</legend></B>
+
+<label for ="name">Name   &nbsp;&nbsp;&nbsp;&nbsp;:</label>
+<input type="text" name="name"><br><br>
+<hr>
+
+<label for ="name">Email   &nbsp;&nbsp;&nbsp;&nbsp;  :</label>
+<input type="email" name="email"><br><br>
+<hr>
+
+<label for ="name">User Name &nbsp;&nbsp;&nbsp;&nbsp;:</label>
+<input type="user name" name=" user name"><br><br>
+<hr>
+
+<label for ="name">Password  &nbsp;&nbsp;&nbsp;&nbsp; :</label>
+<input type="password" name="password"><br><br>
+<hr>
+
+<label for ="name">Confirm Password  &nbsp;&nbsp;&nbsp;&nbsp; :</label>
+<input type="confirm password" name="confirm password"> <hr><br><br>
+
+<fieldset>
+<legend>Gender</legend>
+<input type="radio" name="Male">
+<label for="None">Male</label>
+
+    <input type="radio" name="Female">
+<label for="None">Female</label>
+
+<input type="radio" name="Other">
+<label for="None">Other</label><br><br>
+</fieldset><br>
+<fieldset>
+<legend>Date of Birth</legend>
+<input type="text" name="dname">/
+<input type="text" name="mname">/
+<input type="text" name="yname">
+<label for="name">(dd/mm/yyyy)</label>
+
+
+</fieldset><br>
+<button>Submit</button>
+<button>Reset</button>
+
+
+
+
+
+
+
+
+</fieldset>
+
+</body>
+</html>
